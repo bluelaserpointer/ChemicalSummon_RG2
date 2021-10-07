5145c50f9bd13468f916f2973fc7659e28cb1073
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-10-07 18:09:52 +0800

    remove: UI Effect plugin

--- ChemicalSummon/Assets/GameSystem/World/FairySpot/FairySpot.cs
+++ ChemicalSummon/Assets/GameSystem/World/FairySpot/FairySpot.cs
@@ -10,15 +10,13 @@ public class FairySpot : AbstractWorldEventObject
     [SerializeField]
     float generateSpanSec = 600;
     [SerializeField]
-    new ParticleSystem particleSystem;
+    ParticleSystem avaliableEffect, rootEffect;
 
     float passedTime;
     public bool IsRootable { get; protected set; }
     protected override void Start()
     {
         base.Start();
-        var emmision = particleSystem.emission;
-        emmision.enabled = false;
     }
     protected override void Update()
     {
@@ -32,20 +30,20 @@ public class FairySpot : AbstractWorldEventObject
     public void GenerateFairy()
     {
         IsRootable = true;
-        var emmision = particleSystem.emission;
-        emmision.enabled = true;
+        avaliableEffect.Play();
     }
     public void RootFairy()
     {
         PlayerSave.SubstanceStorage.Add(substance);
         IsRootable = false;
-        var emmision = particleSystem.emission;
-        emmision.enabled = false;
+        avaliableEffect.Stop();
+        rootEffect.Play();
     }
 
     protected override void DoEvent()
     {
-        RootFairy();
+        if(IsRootable)
+            RootFairy();
     }
 
     public override void Submit()
