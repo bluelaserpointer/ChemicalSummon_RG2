b8d8eb6bda97593c3b1fb5d12c5382a2cb2c38e5
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-11-10 10:57:21 +0800

    feat: 反应式搜索加入区别左式/触媒/右式/全部 的4模式

--- ChemicalSummon/Assets/GameSystem/GameTab/ReactionScreen/ReactionListDisplay/ReactionListDisplay.cs
+++ ChemicalSummon/Assets/GameSystem/GameTab/ReactionScreen/ReactionListDisplay/ReactionListDisplay.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -11,22 +13,77 @@ public class ReactionListDisplay : MonoBehaviour
     InputField searchInputField;
     [SerializeField]
     Transform buttonListTransform;
+    [SerializeField]
+    ToggleGroup searchModeToggleGroup;
+    [SerializeField]
+    Toggle leftMode, catalystMode, rightMode, allMode;
 
-    List<FusionButton> originalButtons;
+    List<FusionButton> originalButtons = new List<FusionButton>();
 
-    public void OnSearchFieldChanged()
+    public void OnInputSearchField()
     {
         if (searchInputField.isFocused && General.CurrentSceneIsWorld)
             WorldManager.Player.LockMovement = true;
+        UpdateList();
+    }
+    public void OnSearchModeToggled(bool value)
+    {
+        if (value)
+            UpdateList();
+    }
+    public void UpdateList()
+    {
         string searchStr = searchInputField.text;
         if (searchStr.Length == 0)
-            originalButtons.ForEach(button => button.gameObject.SetActive(true));
-        else
         {
-            foreach(FusionButton button in originalButtons)
+            originalButtons.ForEach(button => button.gameObject.SetActive(true));
+            return;
+        }
+        //because reaction formula does not have fixed ordering between items, we need search each of them
+        string[] searchNames = searchStr.Split(new char[] { '+' }, StringSplitOptions.RemoveEmptyEntries);
+        if(searchNames.Length == 0)
+        {
+            originalButtons.ForEach(button => button.gameObject.SetActive(true));
+            return;
+        }
+        //search mode
+        Func<string, string> searchPlaceConverter;
+        Toggle activeToggle = searchModeToggleGroup.GetFirstActiveToggle();
+        if (activeToggle.Equals(allMode))
+            searchPlaceConverter = (str) => str;
+        else if(activeToggle.Equals(leftMode))
+            searchPlaceConverter = (str) => str.Split('=')[0];
+        else if (activeToggle.Equals(catalystMode))
+            searchPlaceConverter = (str) => str.Split('=')[1];
+        else if (activeToggle.Equals(rightMode))
+            searchPlaceConverter = (str) => str.Split('=')[2];
+        else
+            return;
+        foreach (FusionButton button in originalButtons)
+        {
+            string searchPlace = searchPlaceConverter.Invoke(button.Reaction.formula);
+            bool hit = true;
+            foreach (string searchName in searchNames)
             {
-                button.gameObject.SetActive(button.Reaction.formula.IndexOf(searchStr, System.StringComparison.OrdinalIgnoreCase) >= 0);
+                int hitIndex = searchPlace.IndexOf(searchName, StringComparison.OrdinalIgnoreCase);
+                if (hitIndex == -1)
+                {
+                    hit = false;
+                    break;
+                }
+                //search hole word (exp. don't search "Fe2O3" by "Fe")
+                int nextLetterIndex = hitIndex + searchName.Length;
+                if(searchPlace.Length > nextLetterIndex)
+                {
+                    char nextLetter = searchPlace[nextLetterIndex];
+                    if(!nextLetter.Equals('+') && !nextLetter.Equals('='))
+                    {
+                        hit = false;
+                        break;
+                    }
+                }
             }
+            button.gameObject.SetActive(hit);
         }
     }
     public void DoneSearhFieldEdit()
